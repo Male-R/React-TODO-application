7e02719ecbd6c257a7c06c44c9403b18de15a5ca
Maria Rada <male2593@gmail.com>
2022-07-09 21:57:30 +0000

    adding css

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -21,8 +21,10 @@ const Todo = () => {
 	}
 
 	return (
-		<div className="container center">
-			<h1>ToDo's</h1>
+		<div className="container d-flex flex-column">
+			<div className="text-center">
+				<h1>ToDo's List</h1>
+			</div>
 			<form onSubmit={handleSubmit}>
 				<input
 					onChange={(e) => { setInputValue(e.target.value) }}
@@ -32,7 +34,7 @@ const Todo = () => {
 				/>
 				<ul className="list-group">
 					{taskList.map((task, index) => (
-						<li key={index} className="list-group-item">
+						<li key={index} className="list-group-item d-flex justify-content-between">
 							<span>
 								{task}
 							</span>
